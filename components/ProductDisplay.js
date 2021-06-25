d3020f5b9f3df4ba4979a3590ef91ac256031f41
Sara-Rodrigues <sara.slcr67@gmail.com>
2021-06-24 21:35:36 -0300

    Forms & v-model

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -47,6 +47,8 @@ app.component('product-display', {
           </button>
         </div>
       </div>
+      <review-list v-if="reviews.length" :reviews="reviews"></review-list>
+      <review-form @review-submitted="addReview"></review-form>
     </div>`,
     data() {
         return {
@@ -57,7 +59,8 @@ app.component('product-display', {
             variants: [
                 { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
                 { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-            ]
+            ],
+            reviews: []
         }
     },
     methods: {
@@ -69,6 +72,9 @@ app.component('product-display', {
         },
         updateVariant(index) {
             this.selectedVariant = index
+        },
+        addReview(review) {
+            this.reviews.push(review)
         }
     },
     computed: {
